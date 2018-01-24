989a20fab1c9d7e451fda38fd5e0b7f67e99e832
野沢 貴教 <ponpu1601@outlook.jp>
2018-01-24 11:20:55 +0900

    ViewModelを追加

---
+++ GridLiner/GridLinerViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace GridLiner
+{
+    class GridLinerViewModel:BindableBase
+    {
+
+        private string _ImagePath;
+
+        public string ImagePath
+        {
+            get { return _ImagePath; }
+            set { SetProperty(ref _ImagePath , value); }
+        }
+
+        public GridLinerViewModel()
+        {}
+
+        public GridLinerViewModel(string path)
+        {
+            this.ImagePath = path;
+        }
+
+    }
+}
